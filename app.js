6358c2b87784d9d43fd9713e36df8c5edc909a19
Shubham Meshram <meshram.shubham27@gmail.com>
2022-05-15 19:28:53 +0530

    added stying for button and fetch for getting pokemon info

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Poke-Cards');
+console.log("Poke-Cards");
+const url = "https://pokeapi.co/api/v2/pokemon/";
+const card = document.getElementById("card");
+const button = document.getElementById("button");
+
+let getPokemonData = () => {
+  let randomNumber = Math.floor((Math.random() * 150) + 1);
+  console.log(randomNumber);
+  fetch(url).then( (response) => {
+      console.log(response.json())
+  })
+};
+
+button.addEventListener("click", getPokemonData);
