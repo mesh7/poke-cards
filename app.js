35ca5c40c557b93489affbeaaa391961ec01a58d
Shubham Meshram <meshram.shubham27@gmail.com>
2022-05-15 23:15:13 +0530

    added fetch to consume api and to handle error

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 console.log("Poke-Cards");
-const url = "https://pokeapi.co/api/v2/pokemon/";
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
 const card = document.getElementById("card");
 const button = document.getElementById("button");
 
 let getPokemonData = () => {
-  let randomNumber = Math.floor((Math.random() * 150) + 1);
+  let randomNumber = Math.floor(Math.random() * 150 + 1);
   console.log(randomNumber);
-  fetch(url).then( (response) => {
-      console.log(response.json())
-  })
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        response.json();
+        console.log("Successful", response);
+      } else {
+        console.log("Not successful");
+      }
+    })
+    .then((data) => console.log(data))
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
-button.addEventListener("click", getPokemonData);
+button.addEventListener("click", getPokemonData );
